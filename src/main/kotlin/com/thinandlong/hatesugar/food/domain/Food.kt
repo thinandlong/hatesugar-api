325ac4996614a1aa4ef27eebc5f59a193686a6d2
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-10-09 13:58:55 +0900

    fix: food 정보 수정

--- src/main/kotlin/com/thinandlong/hatesugar/food/domain/Food.kt
+++ src/main/kotlin/com/thinandlong/hatesugar/food/domain/Food.kt
@@ -1,6 +1,7 @@
 package com.thinandlong.hatesugar.food.domain
 
 import com.thinandlong.hatesugar.common.domain.BaseEntity
+import org.hibernate.annotations.Comment
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -12,5 +13,31 @@ class Food(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = -1,
 
-    val name: String
+    val name: String,
+
+    val totalCalories: Double,
+
+    @Comment("탄수화물")
+    val carbohydrate: Float,
+
+    @Comment("단백질")
+    val protein: Float,
+
+    @Comment("지방")
+    val fat: Float,
+
+    @Comment("당류")
+    val sugars: Float,
+
+    @Comment("나트륨")
+    val natrium: Float,
+
+    @Comment("콜레스트레롤")
+    val cholesterol: Float,
+
+    @Comment("포화지방산")
+    val saturatedFatty: Float,
+
+    @Comment("트랜스지방산")
+    val transFatty: Float
 ) : BaseEntity()
